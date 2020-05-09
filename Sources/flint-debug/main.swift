265f7d91a169c3e8ec054e44802e9236b70d2652
Noel Lee <kyl116@ic.ac.uk>
2020-05-09 15:53:04 +0100

    Refactored debugger

--- Sources/flint-debug/main.swift
+++ Sources/flint-debug/main.swift
@@ -8,12 +8,16 @@ import PromiseKit
 func main() {
   command(
       Argument<String>("Transaction hash", description: "Hash of the transaction to be debugged"),
-      Argument<String>("Contract name", description: "Name of the smart contract")
-  ) { txHash, contractName in
-
-    let debugger = Debugger(txHash: txHash, contractName: contractName)
+      Argument<String>("Contract name", description: "Name of the smart contract"),
+      Option<String>("artifacts", default: "bin", description: "Directory containing Flint compiler artifacts"),
+      Option<String>("rpc-url", default: "http://localhost:8545", description: "Ethereum client RPC URL")
+  ) { txHash, contractName, artifactDirectory, rpcURL in
 
     do {
+      let debugger = try DebuggerCLI(txHash: txHash,
+                                     contractName: contractName,
+                                     artifactDirectory: artifactDirectory,
+                                     rpcURL: rpcURL)
       try debugger.run()
     } catch let err {
       print(err)
